f98c644690b1c48160bee0c721a15108a38b084f
TinMH <tinhongmai1012@gmail.com>
2025-08-22 10:51:52 +0700

    first commit

---
+++ src/main/java/vn/host/LoginServlet.java
@@ -0,0 +1,36 @@
+package vn.host;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Cookie;
+
+@WebServlet(urlPatterns= {"/login"})
+public class LoginServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		String user = req.getParameter("username");
+		String pass = req.getParameter("password");
+		if (user.equals("trung") && pass.equals("123")) {
+			Cookie cookie = new Cookie("username", user); 
+			cookie.setMaxAge(30);
+			resp.addCookie(cookie);
+			resp.sendRedirect("/vidu1/hello");
+		} else {
+			resp.sendRedirect(req.getContextPath() + "/login.html");
+		}
+		
+	}
+
+}
---
+++ src/main/java/vn/host/LoginServletSession.java
@@ -0,0 +1,38 @@
+package vn.host;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebServlet(urlPatterns = { "/LoginSession" })
+public class LoginServletSession extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = resp.getWriter();
+		String username = req.getParameter("username");
+		String password = req.getParameter("password");
+
+		if ("trungnh".equals(username) && "123".equals(password)) {
+			out.print("Chào mừng bạn, " + username);
+			HttpSession session = req.getSession();
+			session.setAttribute("name", username);
+		} else {
+			out.print("Tài khoản hoặc mật khẩu không chính xác");
+			req.getRequestDispatcher("LoginSession.html").include(req, resp);
+		}
+	}
+
+}
